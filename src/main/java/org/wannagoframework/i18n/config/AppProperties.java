3faca7f224f2667ae2c2d480cc723297c78a866f
wannagodev1 <wannago.dev1@gmail.com>
2020-04-04 13:40:45 +0100

    Refactoring

--- src/main/java/org/wannagoframework/i18n/config/AppProperties.java
+++ src/main/java/org/wannagoframework/i18n/config/AppProperties.java
@@ -19,18 +19,20 @@
 package org.wannagoframework.i18n.config;
 
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
 @ConfigurationProperties(prefix = "app")
-@Data
 public class AppProperties {
 
   private final Hazelcast hazelcast = new Hazelcast();
-
+  
   @Data
   public static class Hazelcast {
 
     private final ManagementCenter managementCenter = new ManagementCenter();
+    private String interfaces;
     private int timeToLiveSeconds = 3600;
     private int backupCount = 1;
 
